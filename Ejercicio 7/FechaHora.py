3a155a4562f9cbb6b01f52ef87d0dabc321869b2
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2020-04-23 17:55:47 -0300

    Update FechaHora.py

--- Ejercicio 7/FechaHora.py
+++ Ejercicio 7/FechaHora.py
@@ -40,7 +40,7 @@ class FechaHora:
         #calendario
         diasMes = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
         #anio bisiesto
-        if (self.__anio % 4) == 0 : diasMes[2] = 29
+        if (self.__anio % 4 == 0) and ((self.__anio % 100 != 0) or (self.__anio % 400 == 0)) : diasMes[2] = 29
         mes = self.__mes
         anio = self.__anio
 
