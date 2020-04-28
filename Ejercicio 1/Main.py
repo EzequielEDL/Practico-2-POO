49613f6a32cc30c6eb527467c143e7508c235023
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2020-04-28 03:16:49 -0300

    Update Main.py

--- Ejercicio 1/Main.py
+++ Ejercicio 1/Main.py
@@ -12,33 +12,39 @@ def modAccount(oneEmail):
         else:
             print('Invalido')
 
-def readFile():
-    d = input("Ingresar un Dominio: ")
+def loadFile(listaEmail):
     file = open('fileEmail.csv')
     reader = csv.reader(file,delimiter = ',')
-    cont = 0
     i = 0
 
     for fila in reader:
         #print(d)
-        print(fila[i])
-        if(re.search(d, fila[i])) is not None:
-            cont = cont + 1
+        listaEmail.append(fila[i])
         i+1
 
+    file.close()
+
+def findDom(listaEmail):
+    d = input("Ingresar un Dominio: ")
+    cont = 0
+
+    for i in range(len(listaEmail)):
+        if(re.search(d, listaEmail[i])) is not None:
+            cont = cont + 1
+        
     if(cont != 0):
         print('La cantidad de direcciones de e-mail con el dominio ' + d + ' son: ' + str(cont))
     else:
         print('No hay ninguna direccion de e-mail con el dominio ' + d)
 
-    file.close()
+
 
 if __name__ == '__main__':
     n, e, c = input('Nombre: '), input('Cuenta de Correo: '), input('Contraseña: ')
     oneEmail = Email()
     oneEmail.crearCuenta(e,c)
     print('Estimado ' + n + ' te enviaremos tus mensajes a la dirección ' + oneEmail.retornaEmail() + '.')
-
+    
     #Modificar atributo de objeto de Email
     modAccount(oneEmail)
 
@@ -47,4 +53,6 @@ if __name__ == '__main__':
     print('Instancia de ' + otherEmail.retornaEmail())
 
     #Lectura de archivo con atributos de Email
-    readFile()
+    listaEmail = []
+    loadFile(listaEmail)
+    findDom(listaEmail)
