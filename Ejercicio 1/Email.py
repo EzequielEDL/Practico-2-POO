665760e8128f9f6e38a81a4d3ed59d647a9f6ded
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2020-04-28 03:15:32 -0300

    Update Email.py

--- Ejercicio 1/Email.py
+++ Ejercicio 1/Email.py
@@ -1,3 +1,5 @@
+import re
+
 class Email:
     __idCuenta = ''
     __dominio = ''
@@ -10,9 +12,7 @@ class Email:
             self.__dominio = str(dominio)
             self.__tipoDominio = str(tipoDominio)
             self.__clave = str(clave)
-        else :
-            print('Error al validar tipos de datos')
-
+            
     def __testing(self, idCuenta, dominio, tipoDominio, clave):
         if idCuenta.isidentifier() and dominio.isalpha() and tipoDominio.isalpha() and clave.isalnum() :
             return True
@@ -26,16 +26,25 @@ class Email:
         return self.__dominio
 
     def crearCuenta(self, email, clave):
-        s2 = email.split('@')
-        s3 = s2[1].split('.')
+        coutChar1, coutChar2 = 0, 0
 
-        if s2[0].isidentifier() and s3[0].isalpha() and s3[1].isalpha and clave.isalnum() :
-            self.__idCuenta = str(s2[0])
-            self.__dominio = str(s3[0])
-            self.__tipoDominio = str(s3[1])
-            self.__clave = str(clave)
-        else :
-            print('Error al validar tipos de datos')
+        for char in email :
+            if re.search('@', char) is not None :
+                coutChar1 = coutChar1 + 1
+            if re.search('[.]', char) :
+                coutChar2 = coutChar2 + 1
+
+        if coutChar1 == 1 and coutChar2 <= 2 and coutChar2 > 0 :
+            s2 = email.split('@')
+            s3 = s2[1].split('.')
+
+            if s2[0].isidentifier() and s3[0].isalpha() and s3[1].isalpha() and clave.isalnum() :
+                self.__idCuenta = str(s2[0])
+                self.__dominio = str(s3[0])
+                self.__tipoDominio = str(s3[1])
+                self.__clave = str(clave)
+            else :
+                print('Error al validar tipos de datos')
 
     def getClave(self):
         return self.__clave
