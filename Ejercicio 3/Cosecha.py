55e88d92c00f43bc512e85fb075e660df2176245
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2020-04-29 16:36:25 -0300

    Update Cosecha.py

--- Ejercicio 3/Cosecha.py
+++ Ejercicio 3/Cosecha.py
@@ -1,11 +1,27 @@
+import csv
+
 class Cosecha:
-    __kilos = []
+    __listFile = []
 
-    def __init__ (self, kilos = [0, 0, 0]):
-        if type(kilos) == type(self.__kilos) :
-            self.__kilos = kilos
+    def __init__ (self, nameFile = '0', listCamion = []):
+        if nameFile != '0' and type(self.__listFile) == type(listCamion):
+            self.__nameFile = nameFile
+            file = open(nameFile)
+            reader = csv.reader(file, delimiter = ',')
+
+            for row in reader :
+                i = 0
+                while i < len(listCamion) - 1 and listCamion[i].getId() != int(row[0]) :
+                    i = i + 1
+                kilos = int(row[2]) - listCamion[i].getTara()
+                self.__listFile.append([int(row[0]), int(row[1]), kilos])
+
+            file.close()
         else :
-            print('<< Error en la verificacion de tipos de datos >>')  
+            print('\n<< Nombre invalido o archivo inexistente >>')
+        
+    def getFileList(self):
+        return self.__listFile
 
-    def getKilos(self):
-        return self.__kilos
+    def getElementList(self, pos = 0):
+        return self.__listFile[pos]
