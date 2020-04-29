251ddf5ba1e448bdc62aa6aa8b095d35fcc6b1e0
EzequielEDL <64182808+EzequielEDL@users.noreply.github.com>
2020-04-29 12:50:16 -0300

    Update Main.py

--- Ejercicio 2/Main.py
+++ Ejercicio 2/Main.py
@@ -1,54 +1,36 @@
 #Ejercicio 2
 
-from ViajeroFrecuente import ViajeroFrecuente
-import csv
+from ManejadorViajero import ManejadorViajero
 import os
 
-def option1(lista, num):
-    print('\tCantidad total de Millas: ' + str(lista[num-1].cantidadTotaldeMillas()))
+def option1(manejadorViajero, num):
+    print('\tCantidad total de Millas: {}'.format(manejadorViajero.getElementList(num-1).cantidadTotaldeMillas()))
     
     input('\n\n<< press any key to continue >>')
 
-def option2(lista, num):
+def option2(manejadorViajero, num):
     mil = input('\tCantidad de millas: ')
-    lista[num-1].acumularMillas(mil)
+    manejadorViajero.getElementList(num-1).acumularMillas(mil)
     
     input('\n\n<< press any key to continue >>')
 
-def option3(lista, num):
+def option3(manejadorViajero, num):
     mil = input('\tCantidad de millas: ')
-    lista[num-1].canjearMillas(mil)
+    manejadorViajero.getElementList(num-1).canjearMillas(mil)
     
     input('\n\n<< press any key to continue >>')
 
 select = {1: option1, 2: option2, 3: option3}
 
-def menu(opc, lista, num):
+def menu(opc, manejadorViajero, num):
     func = select.get(opc, lambda: print("Opcion Incorrecta"))
-    func(lista, num)
-
-def loadFile(lista):
-    file = open('fileViajero.csv')
-    reader = csv.reader(file,delimiter = ',')
-
-    for fila in reader:
-        viajero = ViajeroFrecuente(fila[0], fila[1], fila[2], fila[3], fila[4])
-        lista.append(viajero)
-        #print(fila)
-    file.close()
-
-def listaMemoria(lista):
-    print('\tAlmacenamiento en Memoria')
-    for i in range(0,len(lista)):
-        print(' {}'.format(lista[i]))
+    func(manejadorViajero, num)
 
 if __name__ == '__main__':
-    lista = []
-    loadFile(lista)    
-    flag = False
 
-    os.system('cls')
-    listaMemoria(lista)
+    flag = False
+    manejadorViajero = ManejadorViajero('fileViajero.csv')
+    print(manejadorViajero)
     input('\n\n<< press any key to continue >>')
 
     while not flag:
@@ -60,8 +42,8 @@ if __name__ == '__main__':
         opc = int(input('\nIngrese Opcion: '))
         if opc != 4 :
             num = int(input("Numero de Viajero frecuente: "))
-            if((num <= len(lista)) and (num > 0)):
-                menu(opc, lista, num)
+            if((num <= len(manejadorViajero.getFileList())) and (num > 0)):
+                menu(opc, manejadorViajero, num)
         else : flag = True
             
     
